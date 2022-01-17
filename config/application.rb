4a1270b6dfc32b2ac204542b87cdb15174b342d3
Saurabh Lodha <saurabhlodha11@gmail.com>
2022-01-17 18:54:55 +0530

    Update test suite to be rspec

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module VendingMachine
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Update test unit to be rspec
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
